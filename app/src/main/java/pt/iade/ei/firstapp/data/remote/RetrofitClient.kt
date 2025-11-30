e29235a4f54636d01b1a3e1f59017fc61e958ad4
fwogga <Miguelalmeida91120@gmail.com>
2025-11-30 04:11:58 +0000

    suporte a telefone real

--- app/src/main/java/pt/iade/ei/firstapp/data/remote/RetrofitClient.kt
+++ app/src/main/java/pt/iade/ei/firstapp/data/remote/RetrofitClient.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080"
+    private const val BASE_URL = "http://127.0.0.1:8080"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
